abf34b71e09ffb7d788a4a9cc67b0cf59aa45219
Gianhunter00 <andrea00@botti.org>
2022-01-22 16:57:09 +0100

    Imported Player and animation
    Added player and animation

---
+++ GJ22/Assets/AnimationManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+
+public class AnimationManager : MonoBehaviour
+{
+    Animator MyAnimator;
+    void Start()
+    {
+        MyAnimator = GetComponent<Animator>();
+        if (MyAnimator == null)
+            throw new System.Exception("Animator NULL");
+        
+    }
+
+
+    void Update()
+    {
+        //float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        MyAnimator.SetFloat("Speed", Mathf.Abs(y));
+
+        transform.position += transform.forward * y;
+
+
+
+    }
+}
