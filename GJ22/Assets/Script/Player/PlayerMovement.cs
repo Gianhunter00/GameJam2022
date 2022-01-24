e4aeb276b84a2f71e84cf65b1a38d4eace9ad1b4
Gianhunter00 <andrea00@botti.org>
2022-01-24 20:15:20 +0100

    Added Player and test map
    Added player sprite and behavior and a test scene to try it

---
+++ GJ22/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator Animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool isFalling;
+    bool jump = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+        Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            Animator.SetBool("IsJumping", true);
+        }
+        if (controller.m_Rigidbody2D.velocity.y < 0 && !controller.m_Grounded && !controller.m_Wall)
+        {
+            Animator.SetBool("IsJumping", false);
+            Animator.SetBool("IsFalling", true);
+            isFalling = true;
+        }
+    }
+    public void Onlanding()
+    {
+        Animator.SetBool("IsJumping", false);
+        Animator.SetBool("IsFalling", false);
+        Animator.SetBool("IsSliding", false);
+        isFalling = false;
+    }
+
+    public void OnWall()
+    {
+        Animator.SetBool("IsSliding", true);
+        Animator.SetBool("IsFalling", false);
+        Animator.SetBool("IsJumping", false);
+    }
+
+    public void NotWall()
+    {
+        Animator.SetBool("IsSliding", false);
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
